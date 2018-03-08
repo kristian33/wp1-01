d9d84009ccc291bf2fd3ea2511882693ed26ede3
kristianklimek <klimekkristian@gmail.com>
2018-03-08 15:00:21 +0100

    přidat index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+</head>
+
+<body>
+<?php
+$PI = "3.14"
+define ("PI", 3.14);
+$r = 5;
+$S = PI * $r * $r;
+define ("r", 5);
+echo "S = PI * r * r =",PI "*$r * $r = $S";
+echo ""
+
+
+?>
+</body>
+
+</html>
